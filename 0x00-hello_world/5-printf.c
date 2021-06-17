12f2119b52379e2664d3de1f20df06e1b6780573
Nmesoma-solomon-peter <nmesomasolo2809@gmail.com>
2021-06-17 11:55:40 -0700

    5-print.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,10 @@
 /**
  *main- prints a text
  *
- *return: always zero
+ *Return: 0
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
- 
+return (0); 
 }

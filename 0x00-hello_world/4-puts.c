62ebfe4fe25ed754e17c62a823a192537f1f4ea0
Nmesoma-solomon-peter <nmesomasolo2809@gmail.com>
2021-06-17 10:23:01 -0700

    4puts.c was added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,10 @@
 
 /* main - prints "programming is like a multilingual  puzzle,
  *
+ *Description: usigning the main function
  *Return: Always ends with 0;
  */
-int main(void)
+ int main(void)
 {
               puts("\"Programming is like building a multilingual puzzle");
 

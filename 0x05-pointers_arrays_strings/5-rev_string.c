02004d8542c8f02951a58a600b7b06a48d04979a
Nmesoma-solomon-peter <nmesomasolo2809@gmail.com>
2021-06-28 18:07:52 +0300

    debugged and recoded task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,33 +1,20 @@
 #include "holberton.h"
 
 /**
-  * rev_string - Reverses a string
-  * @s: The string to be modified
-  *
-  * Return: void
-  */
-void rev_string(char *s)
+ * string_toupper - changes all lowercase letters to uppercase
+ * @s: string to change
+ *
+ * Return: address of s
+ */
+char *string_toupper(char *s)
 {
-	int i, c, k;
-	char *a, aux;
+	int i = 0;
 
-	a = s;
-
-	while (s[c] != '\0')
+	while (*(s + i))
 	{
-		c++;
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+			*(s + i) -= 'a' - 'A';
+		i++;
 	}
-
-	for (k = 1; k < c; k++)
-	{
-		a++;
-	}
-
-	for (i = 0; i < (c / 2); i++)
-	{
-		aux = s[i];
-		s[i] = *a;
-		*a = aux;
-		a--;
-	}
+	return (s);
 }
